c344274059d4d3a2571234fcc9349ab2bef5d6b3
afnavarro27 <andresfelipenavarrovalencia@gmail.com>
2022-05-19 16:08:52 -0500

    Modifique los botones de listar

--- spring-boot-data-jpa/src/main/resources/import.sql
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -2,4 +2,4 @@ INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(1, 'Pepito',
 INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(2, 'Pepe', 'Pipo', 'ppipo@gmail.com','2020-12-21');
 
 INSERT INTO producto(id, productonombre, CreateProducto, cantidad, precio) VALUES(1, 'Carritos', '2020-15-08',12,23);
-INSERT INTO producto(id, productonombre, CreateProducto, cantidad, precio) VALUES(1, 'Moticos', '2020-14-08',15,30);
+INSERT INTO producto(id, productonombre, CreateProducto, cantidad, precio) VALUES(2, 'Moticos', '2020-14-08',15,30);
